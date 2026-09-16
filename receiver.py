99148f9531cf7ee63c8785bacef551acc3ac7b3a
Mohammed Babiker <mbabiker692@gmail.com>
2026-09-15 19:13:49 -0700

    Add timeout and retransmission for lost ACKs

--- receiver.py
+++ receiver.py
@@ -1,24 +1,24 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("127.0.0.1", 5000))
+first_packet = True
 
-packet = sock.recvfrom(1024)
+while True:
+    packet = sock.recvfrom(1024)
+    rawReceivedData = packet[0]
+    receivedData = rawReceivedData.decode()
+    splitData = receivedData.split("|", maxsplit=1)
 
-rawReceivedData = packet[0]
-receivedData = rawReceivedData.decode()
-splitData = receivedData.split("|", maxsplit=1)
-
-seq_num = splitData[0]
-data = splitData[1]
-
-
-returnACK = "ACK " + str(seq_num)
-enc_ACK = returnACK.encode()
-
-
-print("Sequence Number: " + str(seq_num))
-print("Data: " + data)
-print("Packet came from:" , packet[1])
-
-
-sock.sendto(enc_ACK, (packet[1]))
+    seq_num = splitData[0]
+    data = splitData[1]
+    
+    if first_packet:
+        print("Dropping ACK for packet", seq_num)
+        first_packet = False
+    else:
+        returnACK = "ACK " + str(seq_num)
+        enc_ACK = returnACK.encode()
+        print("Sequence Number: " + str(seq_num))
+        print("Data: " + data)
+        print("Packet came from:" , packet[1])
+        sock.sendto(enc_ACK, packet[1])
--- sender.py
+++ sender.py
@@ -1,5 +1,6 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2)
 
 seq_num = 0
 text = "Hello!"
@@ -8,8 +9,25 @@ datapacket = str(seq_num) + "|" + text
 
 data = datapacket.encode()
 
-sock.sendto(data, ("127.0.0.1", 5000))
 
-ACKpacket = sock.recvfrom(1024)
+ack_received = False
+while ack_received == False:
+    sock.sendto(data, ("127.0.0.1", 5000))
+
+    try:
+
+        ACKpacket = sock.recvfrom(1024)
+        rawReceivedACK = ACKpacket[0]
+        receivedACK = rawReceivedACK.decode()
+        ackData = receivedACK.split("ACK ", maxsplit=1)
+        ackseq_num = int(ackData[1])
+        if ackseq_num == seq_num:
+            ack_received = True
+        
+    except TimeoutError:
+        ack_received = False
+        print("Timeout! Retransmitting packet", seq_num)
+
+
 print(ACKpacket[0].decode())
 print(ACKpacket[1])
