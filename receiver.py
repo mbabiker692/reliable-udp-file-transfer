0a7479aa635d1f7b28dc243c1f996c9dbb31f145
Mohammed Babiker <mbabiker692@gmail.com>
2026-09-17 17:28:14 -0700

    Add duplicate packet detection

--- receiver.py
+++ receiver.py
@@ -1,7 +1,9 @@
+#receiver.py
+
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("127.0.0.1", 5000))
-first_packet = True
+expected_seq = 0
 
 while True:
     packet = sock.recvfrom(1024)
@@ -9,16 +11,21 @@ while True:
     receivedData = rawReceivedData.decode()
     splitData = receivedData.split("|", maxsplit=1)
 
-    seq_num = splitData[0]
-    data = splitData[1]
+    seq_num = int(splitData[0])
+
     
-    if first_packet:
-        print("Dropping ACK for packet", seq_num)
-        first_packet = False
+    if seq_num != expected_seq:
+        returnACK = "ACK " + str((expected_seq - 1))
+        enc_ACK = returnACK.encode()
+        sock.sendto(enc_ACK, packet[1])
+        print("Duplicate Packet")
+
     else:
+        data = splitData[1]
         returnACK = "ACK " + str(seq_num)
         enc_ACK = returnACK.encode()
         print("Sequence Number: " + str(seq_num))
         print("Data: " + data)
         print("Packet came from:" , packet[1])
+        expected_seq += 1
         sock.sendto(enc_ACK, packet[1])
--- sender.py
+++ sender.py
@@ -1,3 +1,5 @@
+#sender.py
+
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.settimeout(2)
@@ -15,7 +17,6 @@ while ack_received == False:
     sock.sendto(data, ("127.0.0.1", 5000))
 
     try:
-
         ACKpacket = sock.recvfrom(1024)
         rawReceivedACK = ACKpacket[0]
         receivedACK = rawReceivedACK.decode()
@@ -23,11 +24,30 @@ while ack_received == False:
         ackseq_num = int(ackData[1])
         if ackseq_num == seq_num:
             ack_received = True
+            print(ACKpacket[0].decode())
+            print(ACKpacket[1])
+        
         
     except TimeoutError:
         ack_received = False
         print("Timeout! Retransmitting packet", seq_num)
 
+ack_received = False
+while ack_received == False:
+    sock.sendto(data, ("127.0.0.1", 5000))
 
-print(ACKpacket[0].decode())
-print(ACKpacket[1])
+    try:
+        ACKpacket = sock.recvfrom(1024)
+        rawReceivedACK = ACKpacket[0]
+        receivedACK = rawReceivedACK.decode()
+        ackData = receivedACK.split("ACK ", maxsplit=1)
+        ackseq_num = int(ackData[1])
+        if ackseq_num == seq_num:
+            ack_received = True
+            print(ACKpacket[0].decode())
+            print(ACKpacket[1])
+        
+        
+    except TimeoutError:
+        ack_received = False
+        print("Timeout! Retransmitting packet", seq_num)
